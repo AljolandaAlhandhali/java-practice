b9cdc8bb07f4116a6c2fa2a70dbfb700c9aab750
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-26 21:41:12 +0400

    Add welcome banner message

--- src/AbsoluteValue.java
+++ src/AbsoluteValue.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class AbsoluteValue {
     public static void main(String[] args) {
+        System.out.println("Absolute Value Calculator");
+        System.out.println("========================");
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number: ");
         int n = input.nextInt();
