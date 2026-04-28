6ee282e70b77437871149642c4b70edf98fdc522
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-28 08:06:20 +0400

    Get second number from user

--- src/Modulus.java
+++ src/Modulus.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class Modulus {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter first numbers: ");
         int a = input.nextInt();
+
+        System.out.print("Enter second numbers: ");
+        int b = input.nextInt();
 
     }
 }
