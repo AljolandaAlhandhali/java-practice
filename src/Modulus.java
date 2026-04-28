95b227c4c1bf8a04a662c8bafca1bd1dc6fd8d31
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-28 08:05:40 +0400

    Get first number from user

--- src/Modulus.java
+++ src/Modulus.java
@@ -4,5 +4,8 @@ public class Modulus {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter first numbers: ");
+        int a = input.nextInt();
+
     }
 }
