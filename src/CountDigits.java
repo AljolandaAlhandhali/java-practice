a6044947e471937a8a797b86556e38522be3866f
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-29 19:20:19 +0400

    Read input as String to support large numbers

--- src/CountDigits.java
+++ src/CountDigits.java
@@ -4,5 +4,8 @@ public class CountDigits {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter number: ");
+        String n = input.next();
+
     }
 }
