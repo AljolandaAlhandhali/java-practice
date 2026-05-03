93ec1bf19de5bc58142a598e453db371f370397b
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-03 21:34:36 +0400

    Add input prompt message

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class EvenOdd {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
 
     }
 }
