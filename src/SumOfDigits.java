7f9ff0ec5c5dde7be0a7f899168af26b7a6d017b
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 21:15:07 +0400

    Add prompt message for number input

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class SumOfDigits {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+
     }
 }
