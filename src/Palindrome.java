46101be598643bd9082bd579c0046a71de49e048
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-10 14:50:32 +0400

    Add prompt message for user input

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
     }
 }
