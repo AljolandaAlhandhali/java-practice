91f09cf6de50d1fa329235a179c8f90cc2f77e52
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-15 23:20:17 +0400

    Add message asking user to enter a number

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class ReverseNumber {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
 
     }
 }
